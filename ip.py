8a4b102d1aedb22f655a3fe9e63ee9d654cdbda7
Elio <elionaoum.boujaoudeh@gmail.com>
2022-05-05 11:15:26 +0300

    Removing matplotlib

--- ip.py
+++ ip.py
@@ -9,7 +9,6 @@ from flask import request
 import datetime
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
